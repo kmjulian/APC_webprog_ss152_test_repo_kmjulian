21289ee4ab2a8b78ecb450e02579f26247b4ca84
kmjulian <kylajuliannn@gmail.com>
2016-12-09 23:35:04 +0800

    Tried viewing from using Users controller (not working)

---
+++ Exercise6/codeigniter/application/controllers/Users.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Users extends CI_Controller {
+  function __construct(){
+    parent::__construct();
+    $this->load->helper('url');
+    $this->load->model('users_model');
+  }
+
+  public function add_form(){
+    $this->load->view('insert.php');
+  }
+  
+}
