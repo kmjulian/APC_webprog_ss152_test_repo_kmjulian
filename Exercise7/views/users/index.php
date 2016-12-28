40a1a1ae4819734e645ad154c106654d53510234
kmjulian <kylajuliannn@gmail.com>
2016-12-28 20:51:35 +0800

    created CRUD form for Form(users)

--- Exercise7/views/users/index.php
+++ Exercise7/views/users/index.php
@@ -1,9 +1,40 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>users/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UsersSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="users-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Users', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'user_id',
+            'first_name',
+            'last_name',
+            'nickname',
+            'email:email',
+            // 'user_city',
+            // 'gender',
+            // 'mobile',
+            // 'comment',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
